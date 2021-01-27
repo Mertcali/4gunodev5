0d2bbf617923ebf9d9f5c704cafe534f22d58f46
Ahmet Mert ÇALIŞ <calisahmetmert@gmail.com>
2021-01-27 19:46:57 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                values[j] = tempValues[j];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+
+        // listede oluşturduğumuz tüm işlemleri aslında 2 kere yaptık. hem keys hem values için. Sondaki return işlemlerini hoca derste anlatmadı ancak
+        //ödev olarak verdiği vidyolarda anlatıyor.
+
+
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> magazaUrunler = new MyDictionary<string, string>();
+
+            magazaUrunler.Add("Huawei", "1000");
+            magazaUrunler.Add("Samsung", "2000");
+            magazaUrunler.Add("Apple", "3000");
+
+            foreach (var keys in magazaUrunler.Keys)
+            {
+                Console.WriteLine(keys);
+            }
+            foreach (var values in magazaUrunler.Values)
+            {
+                Console.WriteLine(values);
+            }
+
+            // bunları daha düzgün dursun diye yan yana da yazdırabilirsiniz.
+
+        }
+
+   
+
+
+    }
+}
